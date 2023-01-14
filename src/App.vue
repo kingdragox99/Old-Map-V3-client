6f5b3a3d3e508482325c861b653222357662a5bc
Arthur Lefevre <kingdragox99@gmail.com>
2023-01-14 17:44:07 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <header></header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <h1 class="text-center bg-slate-400">it's work</h1>
+  </main>
+</template>
+
+<script setup></script>
