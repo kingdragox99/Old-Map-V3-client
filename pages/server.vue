b0ef205ad60e2d6bc3e7b9ed67b956eae4a1b86d
Arthur Lefevre <kingdragox99@gmail.com>
2023-04-28 04:45:48 +0200

    feat: fix type error, query server info

--- pages/server.vue
+++ pages/server.vue
@@ -2,9 +2,14 @@
 Header
 main.px-8
   h1 server
+  p 
 Footer
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import query from 'source-server-query';
+
+const datas = query.info('46.105.55.3', 27015, 1000).then(console.log);
+</script>
 
 <style scoped></style>
